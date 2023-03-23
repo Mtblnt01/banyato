b00917c17566c1b003667f959d5f506755b4ba45
Mtblnt01 <126462765+Mtblnt01@users.noreply.github.com>
2023-03-23 10:41:17 +0100

    4es feladat kesz

--- banyato.py
+++ banyato.py
@@ -17,4 +17,92 @@ for melyseg_sor in melysegek: #Így szépen írja ki minden lista 1-1sor.
             print(f"{Back.RESET}{Fore.RESET}{melyseg:2d}", end=" ")
     print() #ezis hogy szépen írja ki
 
-    
+# 2. feladat
+# A mérés sorának azonosítója=12
+# A mérés oszlopának azonosítója=6
+# A mért mélység az adott helyen 33 dm
+print("2.Feladat")
+
+be_sor=int(input("A mérés sorának azonosítója=") or "12")
+be_oszlop=int(input("A mérés oszoplának azonosítója=") or "6")
+print(f"A mért mélység az adott helyen {melysegek[be_sor-1][be_oszlop-1]}dm")
+# 3. feladat
+# A tó felszíne: 646 m2, átlagos mélysége: 4,28 m
+
+print("3.Feladat")
+def megszamolas(m):
+    darab=0
+    for seged_lista in m:
+        for elem in seged_lista:
+            if elem>0:
+                darab+=1
+    return darab
+
+def atlagolas(m):
+    osszeg=0
+    db=0
+    for seged_lista in m:
+        for elem in seged_lista:
+            if elem>0:
+                db+=1
+                osszeg+=elem
+    return osszeg/db
+
+
+
+felszin=megszamolas(melysegek)
+atlagos_melyseg=atlagolas(melysegek)
+print(f"A tó felszíne:{felszin}m2, átlagos mélysége: {atlagolas(melysegek)/10:0.2f}")
+"""
+4. feladat
+Mekkora a tó legnagyobb mélysége, és hol a legmélyebb a tó? Jelenítse meg a választ
+a képernyőn! A legmélyebb pont koordinátáit a mintának megfelelően (sor; oszlop)
+formában írassa ki! Ha több ilyen mérési eredmény is van, mindegyik koordinátapárja
+jelenjen meg!
+"""
+
+
+
+def maximum_kivalasztas(m):
+    max_sor_i=0
+    max_oszlop_i=0
+    for i in range(1,len(m)):
+        for j in range(len(m[i])):
+            if m[max_sor_i][max_oszlop_i]<m[i][j]:
+                max_sor_i=i
+                max_oszlop_i=j
+    return max_sor_i, max_oszlop_i
+
+max_s, max_o=maximum_kivalasztas(melysegek)
+
+
+"""
+def maximum_kivalasztas(lista):
+    max_index=0
+    for index in range(1,len(lista)):
+        if lista[max_index]<lista[index]:
+            max_index=index
+    return max_index
+"""
+print("4. feladat")
+print(f"A tó legnagyobb mélysége: {melysegek[max_s][max_o]} dm")
+#print("A legmélyebb helyek sor-oszlop koordinátái: (14; 20) (26; 11) (32; 16)")
+def legmelyebb_pontok_koordinatai(m, max_ertek):
+    for sor_index,sor in enumerate(m):
+        for oszlop_index,elem in enumerate(sor):
+            if elem==max_ertek:
+                print(f"{sor_index+1}; {oszlop_index}", end=" ")
+
+
+legmelyebb_pontok_koordinatai(melysegek, melysegek[max_s][max_o])
+
+
+
+
+"""
+5. feladat
+Milyen hosszú a tó partvonala, vagyis az ábrán a szürkével jelölt részt határoló vastag fekete
+vonal hossza? A partvonalhoz vegye hozzá a tóban lévő szigetek kerületét is! Írassa ki
+az eredményt a mintának megfelelően a képernyőre! (A megoldás során felhasználhatja,
+hogy a táblázat első és utolsó sorában és oszlopában minden adat 0.) 
+"""
